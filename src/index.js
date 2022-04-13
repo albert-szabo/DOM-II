e3f175d6070db41e22890f5c8739996a052daa0a
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-04-12 18:12:38 -0700

    Completed three additional event listeners

--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ import './less/index.less'
 // Your code goes here!
 
 const title = document.querySelector("h1");
+const homeButton = document.querySelector("#homeButton");
+const aboutUsButton = document.querySelector("#aboutUsButton");
+const blogButton = document.querySelector("#blogButton");
+const contactButton = document.querySelector("#contactButton");
 
 title.addEventListener("mouseover", () => {
     title.style.backgroundColor = 'yellow';
+})
+
+homeButton.addEventListener("click", () => {
+    homeButton.style.backgroundColor = 'green';
+})
+
+aboutUsButton.addEventListener("dblclick", () => {
+    aboutUsButton.style.backgroundColor = 'red';
+})
+
+window.addEventListener("keydown", (event) => {
+    if (event.key === "0") {
+        console.log("You pressed the zero key.");
+    }
 })
