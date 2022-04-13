33c3a8513c90cc872fe179e918cb06f3a98fba7d
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-04-12 17:35:41 -0700

    Completed first event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './less/index.less'
 
 // Your code goes here!
+
+const title = document.querySelector("h1");
+
+title.addEventListener("mouseover", () => {
+    title.style.backgroundColor = 'yellow';
+})
